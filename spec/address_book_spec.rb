505a4c31f998424617371b9583cc7d6d25ceed8e
PRIYAM PATEL <patelpriyam203@gmail.com>
2017-09-17 19:32:21 -0400

    Checkpoint 5 address-bloc menu: Completed

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -18,6 +18,20 @@ RSpec.describe AddressBook do
     end
   end
 
+
+  describe "#remove_entry" do
+    it "removes only one entry from address book" do
+      book = AddressBook.new
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      expect(book.entries.size).to eq(1)
+
+      book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      expect(book.entries.size).to eq(0)
+
+    end
+  end
+
+
   describe "#add_entry" do
     it "adds only one entry to the address book" do
       book = AddressBook.new
